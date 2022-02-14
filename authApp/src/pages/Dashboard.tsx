ef49b7a6dc657be2448f5dd9ba646fc93e467a49
Flavio Santos <flvsantos300@gmail.com>
2022-02-14 09:08:54 -0300

    redirecionamento pelo servidor

--- authApp/src/pages/Dashboard.tsx
+++ authApp/src/pages/Dashboard.tsx
@@ -25,8 +25,10 @@ export default function Dashboard(){
 
 export const getServerSideProps = withSSRAuth(async (ctx) => {
   const apiClient = setupApiClient(ctx)
+  
   const response = await apiClient.get('/me')
   console.log(response.data)
+  
   return{
     props: {}
   }
--- authApp/src/services/api.ts
+++ authApp/src/services/api.ts
@@ -1,6 +1,7 @@
 import axios, { AxiosError } from 'axios'
 import { parseCookies, setCookie } from 'nookies'
 import { signOut } from '../context/AuthContext'
+import { AuthTokenError } from './errors/AuthTokenError'
 
 let isRefreshing = false
 let faileRequestQueue = []
@@ -53,6 +54,8 @@ export function setupApiClient(ctx = undefined) {
   
             if(process.browser){
               signOut()
+            }else{
+              return Promise.reject(new AuthTokenError())
             }
           }).finally(() => {
             isRefreshing = false
