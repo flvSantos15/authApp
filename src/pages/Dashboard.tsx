98b6235b20aefb01c8b3da381c37e6107e6854f1
flvSantos15 <flvSantos300@gmail.com>
2022-03-03 08:26:24 -0300

    demo test

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -8,11 +8,13 @@ import { withSSRAuth } from '../utils/withSSRAuth'
 export default function Dashboard(){
   const { user } = useContext(AuthContext)
   
-  useEffect(() => {
-    api.get(`/me`)
-    .then(response => console.log(response))
-    .catch(err => console.log(err))
-  }, [])
+  // useEffect(() => {
+  //   api.get(`/me`)
+  //   .then(response => console.log(response))
+  //   .catch(err => console.log(err))
+  // }, [])
+
+  // logo vamos criar + coisas
 
   return(
     <Flex p='5' bg='#222f2b'>
